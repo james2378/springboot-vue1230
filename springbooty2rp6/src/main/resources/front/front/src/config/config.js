94e8d6298d1f954b894e4a4b8f13a2d64c26a693
domie1010 <2813169491@qq.com>
2025-03-12 12:38:41 +0800

    Initial commit

---
+++ springbooty2rp6/src/main/resources/front/front/src/config/config.js
@@ -0,0 +1,25 @@
+export default {
+    baseUrl: 'http://localhost:8080/springbooty2rp6/',
+    indexNav: [
+        {
+            name: '首页',
+            url: '/index/home'
+        },
+        {
+            name: '商品信息',
+            url: '/index/shangpinxinxi'
+        },
+        {
+            name: '抢购活动',
+            url: '/index/miaoshashangpin'
+        },
+        {
+            name: '商城资讯',
+            url: '/index/news'
+        },
+        {
+            name: '交流论坛',
+            url: '/index/forum'
+        }, 
+    ]
+}
